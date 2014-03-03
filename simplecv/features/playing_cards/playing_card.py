3c086103bdc82ceea42a4a5eb1d42e55150ce2fd
Kirill Rozin <krozin@mera.ru>
2014-03-03 12:25:58 +0400

    fix pep8 errors in features/playing_cards package

--- simplecv/features/playing_cards/playing_card.py
+++ simplecv/features/playing_cards/playing_card.py
@@ -1,36 +1,35 @@
-from simplecv.base import *
-from simplecv.image_class import *
-from simplecv.color import *
-from simplecv.features.features import Feature, FeatureSet
+from simplecv.features.features import Feature
+
 
 class PlayingCard(Feature):
-    def __init__(self, i, at_x, at_y, rank=None,suit=None):
-
+    def __init__(self, i, at_x, at_y, rank=None, suit=None):
         self.rank = rank
         self.suit = suit
 
-        points = [(at_x-1,at_y-1),(at_x-1,at_y+1),(at_x+1,at_y+1),(at_x+1,at_y-1)]
-        super(PlayingCard, self).__init__(i, at_x, at_y,points)
+        points = [(at_x - 1, at_y - 1), (at_x - 1, at_y + 1),
+                  (at_x + 1, at_y + 1), (at_x + 1, at_y - 1)]
+        super(PlayingCard, self).__init__(i, at_x, at_y, points)
 
-    def getCard(self):
-        return (self.suit,self.rank)
+    def get_card(self):
+        return self.suit, self.rank
 
-    def draw(self, color = (255, 0, 0),width=1):
+    def draw(self, color=(255, 0, 0), width=1):
         """
         **SUMMARY**
 
-        Draw a small circle around the corner.  Color tuple is single parameter, default is Red.
+        Draw a small circle around the corner.  Color tuple is single
+        parameter, default is Red.
 
         **PARAMETERS**
 
         * *color* - An RGB color triplet.
-        * *width* - if width is less than zero we draw the feature filled in, otherwise we draw the
-          contour using the specified width.
+        * *width* - if width is less than zero we draw the feature filled in,
+         otherwise we draw the contour using the specified width.
 
 
         **RETURNS**
 
-        Nothing - this is an inplace operation that modifies the source images drawing layer.
-
+        Nothing - this is an inplace operation that modifies the source images
+        drawing layer.
         """
-        self.image.drawCircle((self.x, self.y), 4, color,width)
+        self.image.drawCircle((self.x, self.y), 4, color, width)
--- simplecv/features/playing_cards/playing_card_factory.py
+++ simplecv/features/playing_cards/playing_card_factory.py
@@ -1,67 +1,68 @@
-from simplecv.base import *
-from simplecv.image_class import *
-from simplecv.color import *
-from simplecv.features.features import Feature, FeatureSet
-from simplecv.features.playing_cards.playing_card import *
+import warnings
+
+from simplecv.features.features import FeatureSet
+from simplecv.features.playing_cards.playing_card import PlayingCard
+
+
 class CardError(Exception):
-    def __init__(self, card=None,message=None):
+    def __init__(self, card=None, message=None):
         self.card = card
         self.msg = message
+
     def __str__(self):
         return repr(self.msg)
 
 
 class PlayingCardFactory():
+    def __init__(self, parameter_dict=None):
+        if parameter_dict is not None:
+            self.parameterize(parameter_dict)
 
-    def __init__(self,parameterDict=None):
-        if(parameterDict is not None):
-            self.parameterize(parameterDict)
-
-    def parameterize(self,parameterDict):
+    def parameterize(self, parameter_dict):
         """
         Parameterize from a dictionary so we can optimize performance.
         """
         pass
 
-    def process(self,img):
+    def process(self, img):
         """
         Process the image. Return a featureset with a single
         PlayingCard feature or None
         """
         # Can we find anything that looks like a card
-        card = self._findCardEdges(img)
-        if( card is None ): # if we don't see it just bail
+        card = self._find_card_edges(img)
+        if card is None:  # if we don't see it just bail
             warnings.warn("Could not find a card.")
             return None
         try:
             # extract the basic features and get color
-            card = self._estimateColor(card)
+            card = self._estimate_color(card)
             # okay, we got a color and some features
             # go ahead and estimate the suit
-            card = self._estimateSuit(card)
+            card = self._estimate_suit(card)
             # Do we think this is a face card this
             # is an easier test
-            isFace,card = self._isFaceCard(card)
-            if(isFace):
+            is_face, card = self._is_face_card(card)
+            if is_face:
                 # if we are a face card get the face. This is had
-                card = self._estimateFaceCard(card)
+                card = self._estimate_face_card(card)
             else:
                 # otherwise get the rank
                 # first pass is corners second
                 # pass is the card body
-                card = self._estimateRank(card)
+                card = self._estimate_rank(card)
             # now go back do some sanity checks
             # and cleanup the features so it is not
             # too heavy
-            card = self._refineEstimates(card)
+            card = self._refine_estimates(card)
         except CardError as ce:
             card = ce.card
-            if( card is not None):
-            # maybe we got a joker or someone
-            # is being a jackass and showing us the
-            # back of the card.
-                card = self._isNonStandardCard(card)
-            warnings.warn(ce.msg) # we may swallow this later
+            if card is not None:
+                # maybe we got a joker or someone
+                # is being a jackass and showing us the
+                # back of the card.
+                card = self._is_non_standard_card(card)
+            warnings.warn(ce.msg)  # we may swallow this later
             # optionally we may want to log these to
             # see where we fail and why or do a parameter
             # adjustment and try again
@@ -72,24 +73,24 @@ class PlayingCardFactory():
             return None
         return FeatureSet([card])
 
-    def _preprocess(self,img):
+    def _preprocess(self, img):
         """
         Any image preprocessing options go here.
         """
         return img
 
-    def _findCardEdges(self,img):
+    def _find_card_edges(self, img):
         """
         Try to find a card, if we do return a card feature
         otherwise return None
         """
         ppimg = self._preprocess(img)
-        retVal = PlayingCard(img,img.width/2,img.height/2)
+        result = PlayingCard(img, img.width / 2, img.height / 2)
         # create the feature, hang any preprocessing
         # steps on the feature
-        return retVal
+        return result
 
-    def _estimateColor(self,card):
+    def _estimate_color(self, card):
         """
         Take in a card feature and determine the color.
         if we find a color return the feature otherwise
@@ -97,7 +98,7 @@ class PlayingCardFactory():
         """
         return card
 
-    def _estimateSuit(self,card):
+    def _estimate_suit(self, card):
         """
         Using the card feature determine suit.
 
@@ -106,26 +107,26 @@ class PlayingCardFactory():
         """
         return card
 
-    def _isFaceCard(self,card):
+    def _is_face_card(self, card):
         """
         Determine if we have a face card
         return any updates to the card and the state.
         """
-        return False,card
+        return False, card
 
-    def _estimateFaceCard(self,card):
+    def _estimate_face_card(self, card):
         """
         Determine if we have a face card K/Q/J and some A
         """
         return card
 
-    def _estimateRank(self,card):
+    def _estimate_rank(self, card):
         """
         Determine the rank and reutrn the card otherwise throw.
         """
         return card
 
-    def _isNonStandardCard(self,card):
+    def _is_non_standard_card(self, card):
         """
         Determine if our card is not a normal card like a joker
         the backside of a card or something similar. Return either the
@@ -133,7 +134,7 @@ class PlayingCardFactory():
         """
         return card
 
-    def _refineEstimates(self,card):
+    def _refine_estimates(self, card):
         """
         Do a post process step if we want.
         e.g. find corners with sub-pix accuracy to
