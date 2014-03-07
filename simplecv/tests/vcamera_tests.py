a385805940420ea1bb1174b0dcf5c228e6ba6baf
Kirill Rozin <krozin@mera.ru>
2014-03-07 14:24:57 +0400

    Correct imports for vcamera_tests.py;

--- simplecv/tests/vcamera_tests.py
+++ simplecv/tests/vcamera_tests.py
@@ -1,15 +1,16 @@
-#!/usr/bin/python
+from nose.tools import nottest
 
-import os, sys
-from simplecv import *
-from nose.tools import with_setup
+from simplecv.camera import VirtualCamera
+from simplecv.image_class import ImageSet
 
 
 testimage = "../data/sampleimages/9dots4lines.png"
 testvideo = "../data/sampleimages/ball.mov"
 testoutput = "../data/test/standard/vc.jpg"
 
-def doFullVCamCoverageTest(vcam):
+
+@nottest
+def do_full_vcam_coverage_test(vcam):
     run = True
     count = 0
     maxf = 1000
@@ -17,18 +18,18 @@ def doFullVCamCoverageTest(vcam):
         img = vcam.getImage()
         vcam.getFrameNumber()
         count = count + 1
-        if( img is None or count > maxf ):
+        if img is None or count > maxf:
             run = False
     vcam.rewind()
     run = True
     while run:
         vcam.skipFrames(2)
-        img = vcam.getImage()  
+        img = vcam.getImage()
         count = count + 1
-        if( img is None or count > maxf ):
+        if img is None or count > maxf:
             run = False
     return True
-    
+
 
 def test_camera_constructor():
     mycam = VirtualCamera(testimage, "image")
@@ -36,40 +37,37 @@ def test_camera_constructor():
 
     for i in props.keys():
         print str(i) + ": " + str(props[i]) + "\n"
-        
-    pass
+
 
 def test_camera_image():
     mycam = VirtualCamera(testimage, "image")
-    if(doFullVCamCoverageTest(mycam)):
-        pass
-    else:
-        assert False
+
+    assert do_full_vcam_coverage_test(mycam)
+
 
 def test_camera_video():
     mycam = VirtualCamera(testvideo, "video")
     img = mycam.getImage()
     img.save(testoutput)
+
+    assert img is not None
     assert img.size() == (320, 240)
-    if(doFullVCamCoverageTest(mycam)):
-        pass
-    else:
-        assert False
+    assert do_full_vcam_coverage_test(mycam)
+
 
 def test_camera_iset():
     iset = ImageSet('../data/test/standard/')
     mycam = VirtualCamera(iset, "imageset")
     img = mycam.getImage()
-    if(doFullVCamCoverageTest(mycam)):
-        pass
-    else:
-        assert False
+
+    assert img is not None
+    assert do_full_vcam_coverage_test(mycam)
+
 
 def test_camera_iset_directory():
     iset = '../data/test/standard/'
     mycam = VirtualCamera(iset, "imageset")
     img = mycam.getImage()
-    if(doFullVCamCoverageTest(mycam)):
-        pass
-    else:
-        assert False
+
+    assert img is not None
+    assert do_full_vcam_coverage_test(mycam)
