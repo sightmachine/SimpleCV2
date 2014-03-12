5c008e138b9e5c0907427c459fd4418f025feafd
Kirill Rozin <krozin@mera.ru>
2014-03-12 13:31:45 +0400

    Renaming methods in machine_learning/shape_context_classifier.py

--- simplecv/machine_learning/shape_context_classifier.py
+++ simplecv/machine_learning/shape_context_classifier.py
@@ -1,10 +1,11 @@
 import math
 import sys
+import warnings
 
 import numpy as np
 
 
-class ShapeContextClassifier():
+class ShapeContextClassifier(object):
     """
     Classify an object based on shape context
     """
@@ -19,35 +20,34 @@ class ShapeContextClassifier():
         * *labels* - the names of each class of objects.
         """
         # The below import has been done in init since it throws
-        # "Need scikits learn installed" for $import SimpleCV
+        # "Need scikits learn installed" for $import simplecv
         try:
             from sklearn import neighbors
-        except:
-            print "Need scikits learn installed"
+        except ImportError:
+            raise ImportError("Need scikits learn installed")
 
-        self.imgMap = {}
-        self.ptMap = {}
-        self.descMap = {}
-        self.knnMap = {}
-        self.blobCount = {}
+        self.img_map = {}
+        self.pt_map = {}
+        self.desc_map = {}
+        self.knn_map = {}
+        self.blob_count = {}
         self.labels = labels
         self.images = images
-        import warnings
 
         warnings.simplefilter("ignore")
         for i in range(0, len(images)):
             print "precomputing " + images[i].filename
-            self.imgMap[labels[i]] = images[i]
+            self.img_map[labels[i]] = images[i]
 
-            pts, desc, count = self._image2FeatureVector(images[i])
-            self.blobCount[labels[i]] = count
-            self.ptMap[labels[i]] = pts
-            self.descMap[labels[i]] = desc
+            pts, desc, count = self._image_to_feature_vector(images[i])
+            self.blob_count[labels[i]] = count
+            self.pt_map[labels[i]] = pts
+            self.desc_map[labels[i]] = desc
             knn = neighbors.KNeighborsClassifier()
             knn.fit(desc, range(0, len(pts)))
-            self.knnMap[labels[i]] = knn
+            self.knn_map[labels[i]] = knn
 
-    def _image2FeatureVector(self, img):
+    def _image_to_feature_vector(self, img):
         """
         generate a list of points, SC descriptors, and the count of points
         """
@@ -63,35 +63,35 @@ class ShapeContextClassifier():
                 raw_descriptors = blobs[0]._generate_sc(fulllist)
         return fulllist, raw_descriptors, count
 
-    def _getMatch(self, model_scd, test_scd):
-        correspondence, distance = self._doMatching(model_scd, test_scd)
-        return self._matchQuality(distance)
+    def _get_match(self, model_scd, test_scd):
+        correspondence, distance = self._do_matching(model_scd, test_scd)
+        return self._match_quality(distance)
 
-    def _doMatching(self, model_name, test_scd):
-        myPts = len(test_scd)
-        otPts = len(self.ptMap[model_name])
+    def _do_matching(self, model_name, test_scd):
+        # my_pts = len(test_scd)
+        # ot_pts = len(self.pt_map[model_name])
         # some magic metric that keeps features
         # with a lot of points from dominating
-        #  this could be moved to after the sum
-        #metric = 1.0 + np.log10( np.max([myPts,otPts])/np.min([myPts,otPts]))
-        otherIdx = []
+        # this could be moved to after the sum
+        # metric = 1.0 + np.log10(np.max([my_pts, ot_pts])
+        #                                / np.min([my_pts, ot_pts]))
+        other_idx = []
         distance = []
-        import warnings
 
         warnings.simplefilter("ignore")
-        results = []
+
         for sample in test_scd:
-            best = self.knnMap[model_name].predict(sample)
+            best = self.knn_map[model_name].predict(sample)
             idx = best[0]  # this is where we can play with k
-            scd = self.descMap[model_name][idx]
+            scd = self.desc_map[model_name][idx]
             temp = np.sqrt(np.sum(((sample - scd) ** 2)))
             #temp = 0.5*np.sum((sample-scd)**2)/np.sum((sample+scd))
             if math.isnan(temp):
                 temp = sys.maxint
             distance.append(temp)
-        return [otherIdx, distance]
+        return [other_idx, distance]
 
-    def _matchQuality(self, distances):
+    def _match_quality(self, distances):
         #distances = np.array(distances)
         #sd = np.std(distances)
         #x = np.mean(distances)
@@ -106,63 +106,63 @@ class ShapeContextClassifier():
         std = np.std(distances)
         return tmean, std
 
-    def _buildMatchDict(self, image, countBlobs):
+    def _build_match_dict(self, image, count_blobs):
         # we may want to base the count on the num best
         # matchesber of large blobs
-        points, descriptors, count = self._image2FeatureVector(image)
-        matchDict = {}
-        matchStd = {}
-        for key, value in self.descMap.items():
+        points, descriptors, count = self._image_to_feature_vector(image)
+        match_dict = {}
+        match_std = {}
+        for key, value in self.desc_map.items():
             # only do matching for similar number of blobs
-            if countBlobs and self.blobCount[key] == count:
+            if count_blobs and self.blob_count[key] == count:
                 #need to hold on to correspondences
-                correspondence, distances = self._doMatching(key, descriptors)
-                result, std = self._matchQuality(distances)
-                matchDict[key] = result
-                matchStd[key] = std
-            elif not countBlobs:
-                correspondence, distances = self._doMatching(key, descriptors)
-                result, std = self._matchQuality(distances)
-                matchDict[key] = result
-                matchStd[key] = std
+                correspondence, distances = self._do_matching(key, descriptors)
+                result, std = self._match_quality(distances)
+                match_dict[key] = result
+                match_std[key] = std
+            elif not count_blobs:
+                correspondence, distances = self._do_matching(key, descriptors)
+                result, std = self._match_quality(distances)
+                match_dict[key] = result
+                match_std[key] = std
 
-        return points, descriptors, count, matchDict, matchStd
+        return points, descriptors, count, match_dict, match_std
 
-    def classify(self, image, blobFilter=True):
+    def classify(self, image, blob_filter=True):
         """
         Classify an input image.
 
         * *image* - the input binary image.
-        * *blobFilter* - Do a first pass where you only match objects
+        * *blob_filter* - Do a first pass where you only match objects
           that have the same number of blobs - speeds up computation
           and match quality.
         """
-        points, descriptors, count, matchDict, matchStd = self._buildMatchDict(
-            image, blobFilter)
+        points, descriptors, count, match_dict, match_std = \
+            self._build_match_dict(image, blob_filter)
         best = sys.maxint
         best_name = "No Match"
-        for k, v in matchDict.items():
+        for k, v in match_dict.items():
             if v < best:
                 best = v
                 best_name = k
 
-        return best_name, best, matchDict, matchStd
+        return best_name, best, match_dict, match_std
 
-    def getTopNMatches(self, image, n=3, blobFilter=True):
+    def get_top_nmatches(self, image, n=3, blob_filter=True):
         """
         Classify an input image and return the top n results.
 
         * *image* - the input binary image.
         * *n* - the number of results to return.
-        * *blobFilter* - Do a first pass where you only match objects
+        * *blob_filter* - Do a first pass where you only match objects
           that have the same number of blobs - speeds up computation
           and match quality.
         """
         n = np.clip(n, 1, len(self.labels))
-        points, descriptors, count, matchDict, matchStd = self._buildMatchDict(
-            image, blobFilter)
-        best_matches = list(sorted(matchDict, key=matchDict.__getitem__))
-        retList = []
+        points, descriptors, count, match_dict, match_std = \
+            self._build_match_dict(image, blob_filter)
+        best_matches = list(sorted(match_dict, key=match_dict.__getitem__))
+        ret_list = []
         for k in best_matches:
-            retList.append((k, matchDict[k]))
-        return retList[0:n], matchDict, matchStd
+            ret_list.append((k, match_dict[k]))
+        return ret_list[0:n], match_dict, match_std
