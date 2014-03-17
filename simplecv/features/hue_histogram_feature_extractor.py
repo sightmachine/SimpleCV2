8ae9ddda72c5953e53cd5ee6038e49a64ebdc393
Betrezen <krozin@gmail.com>
2014-03-17 11:32:36 +0400

    cv->cv2: hue_histogram_feature_extractor

--- simplecv/features/hue_histogram_feature_extractor.py
+++ simplecv/features/hue_histogram_feature_extractor.py
@@ -1,4 +1,4 @@
-from cv2 import cv
+import cv2
 import numpy as np
 
 from simplecv.features.feature_extractor_base import FeatureExtractorBase
@@ -24,9 +24,8 @@ class HueHistogramFeatureExtractor(FeatureExtractorBase):
         color histogram of the pixel counts of each hue.
         """
         img = img.to_hls()
-        empty_img = img.get_empty(1)
-        cv.Split(img.get_bitmap(), empty_img, None, None, None)
-        npa = np.array(empty_img[:, :])
+        empty_img = cv2.split(img.get_ndarray())
+        npa = empty_img[0]
         npa = npa.reshape(1, npa.shape[0] * npa.shape[1])
         hist = np.histogram(npa, self.nbins, normed=True, range=(0, 255))
         return hist[0].tolist()
